325cf91871ebbcc13ad7b5ecff3b86bb60f010fa
QinxuanShi <qinxuan.shi@und.edu>
2026-03-11 22:46:19 -0500

    update files

--- Edge/edge_scripts/predict.sh
+++ Edge/edge_scripts/predict.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-python threshold_predict.py --score_file prediction_results/Openstack_e6_k1_l3_b96_scores.txt --output_pred prediction_results/Openstack_e6_k1_l3_b96_predictions.txt  --dataset os  --energy_name Openstack_e6_k1_l3_b96  --thresholds_yaml threshold_os.yaml  --label_file dataset/OpenStack_label_w100_len10.csv
+python threshold_predict.py --score_file prediction_results/Openstack_e6_k1_l3_b96_scores.txt --output_pred prediction_results/Openstack_e6_k1_l3_b96_predictions.txt  --dataset os  --energy_name Openstack_e6_k1_l3_b96  --thresholds_yaml threshold_os.yaml  --label_file dataset/openstack_label.csv
 
-python threshold_predict.py --score_file prediction_results/Openstack_e10_k5_l3_b64_scores.txt --output_pred prediction_results/Openstack_e10_k5_l3_b64_predictions.txt  --dataset os  --energy_name Openstack_e10_k5_l3_b64  --thresholds_yaml threshold_os.yaml  --label_file dataset/OpenStack_label_w100_len10.csv
+python threshold_predict.py --score_file prediction_results/Openstack_e10_k5_l3_b64_scores.txt --output_pred prediction_results/Openstack_e10_k5_l3_b64_predictions.txt  --dataset os  --energy_name Openstack_e10_k5_l3_b64  --thresholds_yaml threshold_os.yaml  --label_file dataset/openstack_label.csv
 
-python threshold_predict.py --score_file prediction_results/Openstack_e10_k5_l3_b96_scores.txt --output_pred prediction_results/Openstack_e10_k5_l3_b96_predictions.txt  --dataset os  --energy_name Openstack_e10_k5_l3_b96  --thresholds_yaml threshold_os.yaml  --label_file dataset/OpenStack_label_w100_len10.csv
+python threshold_predict.py --score_file prediction_results/Openstack_e10_k5_l3_b96_scores.txt --output_pred prediction_results/Openstack_e10_k5_l3_b96_predictions.txt  --dataset os  --energy_name Openstack_e10_k5_l3_b96  --thresholds_yaml threshold_os.yaml  --label_file dataset/openstack_label.csv
